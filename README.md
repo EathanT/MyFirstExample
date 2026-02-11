764f8039144610ff04480690bf1eafc16f40ec7a
Never :q! <85617435+EathanT@users.noreply.github.com>
2026-02-11 13:37:04 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build C++](https://github.com/msRaquel/MyFirstExample/actions/workflows/actions.yml/badge.svg)](https://github.com/msRaquel/MyFirstExample/actions/workflows/actions.yml)
--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ using std::cout;
 int main()
 {
     cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    cout << "Hi, please enter two whole numbers: ";
+    cout << "Hi, please enter two whole numbers please: ";
 
     int x,y;
 
+<<<<<<< HEAD
     cin >> x >> y;
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
@@ -20,6 +21,17 @@ int main()
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
+=======
+    std::cin >> x >> y;
+    std::cout << "Addition: " << x + y << std::endl;
+    std::cout << "Subtraction: " << x - y << std::endl;
+    std::cout << "Multiplication: " << x * y << std::endl;
+    std::cout << "Division: " << x / y << std::endl;
+    std::cout << "Remainder: " << x % y << std::endl;
+    if(x = 0){ std::cout<<"Cannot divide by 0"<< std::endl;}
+    std::cout << "Square Root: " << sqrt(x) << std::endl;
+    std::cout << "Square: " << pow(x, y) << std::endl;
+>>>>>>> int32_error340
 
     return 0;
 }
